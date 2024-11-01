ccf3f00a70ff3df5ee4f4e4f1513d3fd6e2a3eeb
Your Name <you@example.com>
2024-11-01 18:17:37 +0300

    first commit

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ const express = require('express');
    const mongoose = require('mongoose');
    const cors = require('cors');
    const bcrypt = require('bcryptjs');
-   const User = require('./models/User');
+   const User = require('./models/user');
    const app = express();
    app.use(express.json());
    app.use(cors());
